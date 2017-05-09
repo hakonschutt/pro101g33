1b2ba1b311158fa915fdad841cd93f520848a81e
Hakon Schutt <hakon.schutt@drivtrafikk.no>
2017-05-09 14:27:30 +0200

    changes to style for frontpage

--- core/init.php
+++ core/init.php
@@ -6,5 +6,44 @@
 /*
 /*******************************************************************************************************/
 
+	error_reporting(E_ALL);
+	ini_set('display_errors', '1');
+
     require 'database/db.php';
+
+
+   class Init {
+   	public static function header(){
+
+   		$content = '
+			<!DOCTYPE html>
+			<html lang="no">
+			<head>
+			    <meta charset="utf-8" />
+			    <meta name="author" content="Gruppe 36 - PJ2100">
+				<meta http-equiv="X-UA-Compatible" content="IE=9" />
+				<link rel="shortcut icon" href="img/icon.png" type="image/x-icon" />
+				<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" />
+			    <link rel="stylesheet" href="assets/css/style.css" type="text/css" />
+
+			    <!-- Bruker PHP for å hente tittelen på gjeldende side -->
+			    <title>@Campus</title>
+			</head>
+   		';
+
+   		return $content;
+
+   	} // End header
+
+   	public static function footer(){
+
+   		$content = '
+			</body>
+			</html>
+   		';
+
+   		return $content;
+
+   	} // End footer
+   }
 ?>
--- index.php
+++ index.php
@@ -1,45 +1,45 @@
-</DOCTYPE html>
-<html lang="no">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="author" content="Gruppe 36 - PJ2100">
-		<meta http-equiv="X-UA-Compatible" content="IE=9" />
-		<link rel="shortcut icon" href="img/icon.png" type="image/x-icon" />
-        <link rel="stylesheet" href="css/style.css" type="text/css" />
+<?php
+    require 'core/init.php';
+    echo Init::header();
+?>
 
-        <!-- Bruker PHP for å hente tittelen på gjeldende side -->
-        <title>@Campus</title>
-</head>
-<body>
-    <div class="main-container" style="background-image: url('img/LargePic/Background.png');">
-        <div class="main-container-inner">
-            <div class="left-col">
-                <img class="left-col-logo" src="img/westerdals-hvit.png">
-                <p class="left-col-text">But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful.</p>
+<body class="indexbody" style="background-image: url('img/LargePic/Background.png');">
+    <div class="main-container">
+        <div class="left-col">
+            <div class="left-col-inner">
+                <img class="left-col-inner-logo" src="img/westerdals-hvit.png">
+                <p class="left-col-inner-text">But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful.</p>
             </div>
-            <div class="right-col">
-                <h1 class="campus-title">Velg Campus</h1>
-                <div id="campuses">
-                	<?php 
-                        require 'core/init.php'; 
-                        // Starter queryen.
-                        $query = "SELECT * FROM campus";
-                        $sql = $database->prepare("$query;");
-                        $sql->setFetchMode(PDO::FETCH_OBJ);
-                        $sql->execute();
+        </div>
+        <!--<hr clasS="middle-line">-->
+        <div class="right-col">
+            <h1 class="campus-title">Velg Campus</h1>
+            <div id="campuses">
+            	<?php 
+                    
+                    // Starter queryen.
+                    $query = "SELECT * FROM campus";
+                    $sql = $database->prepare("$query;");
+                    $sql->setFetchMode(PDO::FETCH_OBJ);
+                    $sql->execute();
 
-                        // Kjører en loop for hvert element i som PDO henter.
-                        while ($element = $sql->fetch()) {
-                            echo '
-                                <div id="campus campus' . $element->campus_id . '">
-                                    <img class="campus--icon" src="' . $element->bilde_path . '"/>
-                                    <span class="campusn--ame">' . $element->navn . '</span>
+                    // Kjører en loop for hvert element i som PDO henter.
+                    while ($element = $sql->fetch()) {
+                        echo '
+                            <a href="campus.php?id=' . $element->campus_id . '">
+                                <div class="campus" id="campus' . $element->campus_id . '">
+                                    <img class="campus-icon" src="' . $element->bilde_path . '"/>
+                                    <span class="campus-name">' . $element->navn . '</span>
+                                    <img class="campus-arrow" src="img/arrow-icon.png"/>
                                 </div>
-                            ';
-                        }
-                    ?>
-                </div>
+                            </a>
+                        ';
+                    }
+                ?>
             </div>
         </div>
     </div>
-</body>
+
+<?php
+    echo Init::footer();
+?>
