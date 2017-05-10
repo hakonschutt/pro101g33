8fba7cd25149515ee0ee5923b1b6670047c61a2d
Hakon Schutt <hakon.schutt@drivtrafikk.no>
2017-05-10 02:56:22 +0200

    added info and contact to campus page

--- campus.php
+++ campus.php
@@ -71,18 +71,53 @@ echo '</div>';
 
 ?>
 <div class="info">
-	<div class="info--inner">
-		<div class="info--inner-left">
-	
-
+	<div class="info-inner">
+		<div class="info-inner-left">
+			<div class="info-inner-left-topwrap">
+				<h1>Hva er @Campus</h1>
+				<p>Lorem Ipsum er rett og slett dummytekst fra og for trykkeindustrien. Lorem Ipsum har vært bransjens standard for dummytekst helt siden 1500-tallet, da en ukjent boktrykker stokket en mengde bokstaver for å lage et prøveeksemplar av en bok. Lorem Ipsum har tålt tidens tann usedvanlig godt, og har i tillegg til å bestå gjennom fem århundrer også tålt spranget over til elektronisk typografi uten vesentlige endringer.</p>
+			</div>
+			<div class="info-inner-left-bottomwrap">
+				<h1>Hvorfor bruker vi det?</h1>
+				<p>Det er et velkjent faktum at lesere distraheres av lesbart innhold på en side når man ser på dens layout. Poenget med å bruke Lorem Ipsum er at det har en mer eller mindre normal fordeling av bokstaver i ord, i motsetning til ‘Innhold her, innhold her’, og gir inntrykk av å være lesbar tekst. Mange webside- og sideombrekkingsprogrammer bruker nå Lorem Ipsum som sin standard for provisorisk tekst, og et søk etter ‘Lorem Ipsum’ vil avdekke mang en uferdig webside. Ulike versjoner har sprunget frem i senere år, noen ved rene uhell og andre mer planlagte (med humor o.l.).</p>
+				<p>Det finnes mange ulike varianter av Lorem Ipsum, men majoriteten av disse har blitt tuklet med på et eller annet vis. Det være seg innlagt humor eller tilfeldig genererte ord som ser langt fra troverdige ut. Skal man bruke avsnitt av Lorem Ipsum må man være sikker på at det ikke skjuler seg noe pinlig midt i teksten. </p>
+			</div>
 		</div>
-		<div class="info--inner-right">
-		
-
+		<div class="info-inner-right">
+			<img class="info-inner-right-image" src="img/LargePic/image1.jpg" />
+			<img class="info-inner-right-image" src="img/LargePic/image2.jpg" />
+			<img class="info-inner-right-image" src="img/LargePic/image3.jpg" />
 		</div>
 	</div>
 </div>
 
+<div class="contact" style="background-image: url('img/LargePic/Background.png');">
+	<div class="contact-inner">
+		<h1 class="contact-inner-header">Kontakt oss</h1>
+		<div class="contact-inner-left">
+			<img class="contact-inner-left-image" src="img/LargePic/map.png" />
+			<div class="contact-inner-left-info">
+				<div class="contact-inner-left-info-bit">
+					<img src="img/icons/pin.png" />
+					<p>Chr. Krohgs gate 32, 0186 Oslo</p>
+				</div>
+				<div class="contact-inner-left-info-bit">
+					<img src="img/icons/mail.png" />
+					<p>post@westerdals.no</p>
+				</div>
+				<div class="contact-inner-left-info-bit">
+					<img src="img/icons/phone.png" />
+					<p>22 05 75 50</p>
+				</div>	
+
+			</div>
+		</div>
+		<div class="contact-inner-right">
+
+		</div>
+	</div>
+
+</div>
 
 
 <?php
