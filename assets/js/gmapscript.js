e8cc489f64fdca9ef8576d3d5be4c6edd4302f7e
Håvard Halvorsen <h.halvorsen@live.no>
2017-05-16 15:42:44 +0200

    Fixed and added functions to gmapscript

--- assets/js/gmapscript.js
+++ assets/js/gmapscript.js
@@ -1,58 +1,58 @@
-//Oppretter for øyeblikket kun et ufullstending kart over fjerdingen
+var id = _CAMPUS_ID;
+var markers = [];
+var zoom = 15;
+var centerPos;
 
 var fjerdingen_locations = [
     ['Fjerdingen', 59.916049, 10.760024, 'campus'],
     ['Hausmanns gate', 59.916395, 10.757641, 'transport'],
-    ['Rema 1000', 59.914244, 10.756892, 'mat']
-];
-var fjerdingen_infoContent = [
-    ['<div class="f_info_content">' + '<h3>Fjerdingen</h3>' + '<p>INFO OM FJERDINGEN</p>' + '</div>'],
-    ['<div class="b_info_content">' + '<h3>Hausmanns gate</h3>' + '<p>BUSS OG GREIER</p>' + '</div>'],
-    ['<div class="f_info_content">' + '<h3>Rema 1000</h3>' + '<p>MAT</p>' + '</div>']
+    ['Rema 1000', 59.914244, 10.756892, 'matbutikker']
 ];
 
 //Vulkan
 var vulkan_locations = [
-    ['Vulkan', 59.923341, 10.752494],
-    ['Telthusbakken', 59.924503, 10.750547]
-];
-var vulkan_infoContent = [
-    ['<div class="v_info_content">' + '<h3>Vulkan</h3>' + '<p>INFO OM VULKAN</p>' + '</div>'],
-    ['<div class="v_info_content">' + '<h3>Telthusbakken</h3>' + '<p>BUUSSSS</p>' + '</div>']
+    ['Vulkan', 59.923341, 10.752494, 'campus'],
+    ['Telthusbakken', 59.924503, 10.750547, 'transport']
 ];
 
 //Brenneriveien
 var brenneriveien_locations = [
-    ['Brenneriveien', 59.920360, 10.752799],
-    ['Møllerveien', 59.920611, 10.751407]
-];
-var brenneriveien_infoContent = [
-    ['<div class="b_info_content">' + '<h3>Brenneriveien</h3>' + '<p>INFO OM BRENNERIVEIEN</p>' + '</div>'],
-    ['<div class="b_info_content">' + '<h3>Møllerveien</h3>' + '<p>BUSS!?</p>' + '</div>']
+    ['Brenneriveien', 59.920360, 10.752799, 'campus'],
+    ['Møllerveien', 59.920611, 10.751407, 'transport']
 ];
 
-var iconBasePath = 'WebProsjekt2\png\';
 var icons = [
-    campus: {
-        icon: iconBasePath + '005-puzzle.png'
-    },
-    transport: {
-        icon: iconBasePath + '008-front-of-bus.png'
-    },
-    mat: {
-        icon: iconBasePath + '007-food.png'
-    }
-]
-createMap('fjerdingenMap', 14, fjerdingen_locations, fjerdingen_infoContent);
-//createMap('vulkanMap', 14, vulkan_locations, vulkan_infoContent);
-//createMap('brenneriveienMap', 14, brenneriveien_locations, brenneriveien_infoContent);
+    ["campus", 'img/campus/Fjerdingen.png'],
+    ["bysykkel", 'img/aktivitet/bicycle.png'],
+    ["transport", 'img/aktivitet/front-of-bus.png'],
+    ["matbutikker", 'img/aktivitet/shopping-cart.png'],
+    ["spisesteder", 'img/aktivitet/food2.png'],
+    ["utesteder", 'img/aktivitet/food.png'],
+    ["fritidsaktiviteter", 'img/aktivitet/puzzle.png']
+];
 
 var directionsDisplay;
 var directionsService = new google.maps.DirectionsService();
+var infoWindow;
 
+switch(id) {
+    case "1":
+        centerPos = new google.maps.LatLng(59.916049, 10.760024);
+        createMap(zoom, fjerdingen_locations);
+        break;
+    case "2":
+        centerPos = new google.maps.LatLng(59.923341, 10.752494);
+        createMap(zoom, vulkan_locations);
+        break;
+    case "3":
+        centerPos = new google.maps.LatLng(59.920360, 10.752799);
+        createMap(zoom, brenneriveien_locations);
+        break;
+}
 
-//Lager kart og fyller med markers fra -locations- med info fra -infoContent-
-function createMap(targetDiv, zoom, locations, infoContent) {
+//Lager kart og fyller med markers fra -locations-
+function createMap(zoom, locations, locId) {
+    var i;
     directionsDisplay = new google.maps.DirectionsRenderer({
         preserveViewport: true,
         suppressMarkers: true,
@@ -60,29 +60,69 @@ function createMap(targetDiv, zoom, locations, infoContent) {
             strokeColor : "red"
         }
     });
-    var map = new google.maps.Map(document.getElementById(targetDiv), {
+    var map = new google.maps.Map(document.getElementById('campusmap'), {
         zoom: zoom,
-        center: new google.maps.LatLng(locations[0][1], locations[0][2]),
+        center: centerPos,
     });
     directionsDisplay.setMap(map);
+    infoWindow = new google.maps.InfoWindow();
     
-    var infoWindow = new google.maps.InfoWindow();
-    var marker, i;
-        
-    for(i = 0; i < locations.length; i++) {
-        marker = new google.maps.Marker({
-            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-            icon: icons[locations[i][3]],
-            map: map
-        });
-    
-        google.maps.event.addListener(marker, 'click', (function(marker, i) {
-            return function() {
-                infoWindow.setContent(infoContent[i][0]);
-                infoWindow.open(map, marker);
-                createRoute(new google.maps.LatLng(locations[0][1], locations[0][2]), marker.getPosition());
+    if (locId > 0) {
+        addMarker(locations[locId][0], locations[locId][1], locations[locId][2], locations[locId][3], map);
+        }
+    } else {
+        for(i = 0; i < locations.length; i++) {
+            addMarker(locations[i][0], locations[i][1], locations[i][2], locations[i][3], map);
+        }
+    }
+}
+
+function addMarker(name, lat, lng, category, map) {
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(lat, lng),
+        category: category,  
+        map: map
+    });
+    markers.push(marker);
+    marker.setIcon(new google.maps.MarkerImage(typeToIconPath(marker.category), null, null, null, new google.maps.Size(28, 28)));
+    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+            directionsDisplay.setMap(map);
+            var div = document.getElementById('markercontent');
+            infoWindow.setContent(name);
+            infoWindow.open(map, marker);
+            createRoute(map.getCenter(), marker.getPosition());
+            div.innerHTML = name;
+        }
+    })(marker, i));
+}
+
+function typeToIconPath(type) {
+    for(i = 0; i < icons.length; i++) {
+        var iconType = icons[i][0]
+        if (type == iconType) {
+            return icons[i][1];
+        }
+    }
+    return 'img/icons/pin.png';
+}
+
+
+function filterMarkersByType(type) {
+    if(directionsDisplay != null) {
+        directionsDisplay.setMap(null);
+    }
+    for(i = 0; i < markers.length; i++) {
+        marker = markers[i];
+        if(type == "all") {
+            marker.setVisible(true);
+        } else {
+            if(marker.category == type || type.length === 0 || marker.category == "campus") {
+                marker.setVisible(true);
+            } else {
+                marker.setVisible(false);
             }
-        })(marker, i));
+        }
     }
 }
 
--- map.php
+++ map.php
@@ -30,14 +30,14 @@ if ($error === null){
 
 	// Kjører en loop for hvert element i som PDO henter.
 	while ($element = $sql->fetch()) {
-	    echo '
+	   /* echo '
 	        <a href="campus.php?id=' . $element->campus_id . '">
 	            <div id="campus campus' . $element->campus_id . '">
 	                <img class="campus--icon" src="' . $element->bilde_path . '"/>
 	                <span class="campusn--ame">' . $element->navn . '</span>
 	            </div>
 	        </a>
-	    ';
+	    ';*/
 	}
 }
 
@@ -45,7 +45,26 @@ if ($error === null){
 /*	Room for rest of activity post
 /*
 /***************************************/
+?>
 
-require_once 'core/footer.php';
+<div class="mapcontainer">
+    <div class="map" id="campusmap" style="width:50%;height:400px;"></div>
+    <form>
+        <div>
+            <input type="radio" name="a" value="all" onclick="filterMarkersByType(this.value);" checked>
+            <input type="radio" name="a" value="transport" onclick="filterMarkersByType(this.value);">
+            <input type="radio" name="a" value="matbutikker" onclick="filterMarkersByType(this.value);">
+        </div>
+    </form>
+    <div class="markerinfo" id="markercontent"></div>
+</div>
 
-echo Init::footer(); 
+
+<script type"text/javascript">var _CAMPUS_ID = "<?= $id; ?>";</script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA-eHlKwovIr9zf4zPpvNlaDazU5OKWeKk"></script>
+<script src="assets/js/gmapscript.js"></script>
+    
+<?php
+    require_once 'core/footer.php';
+    echo Init::footer(); 
+?>
