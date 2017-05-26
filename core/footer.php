0130ef4f38bb532289166965c6663d05176acda3
Hakon Schutt <hakon.schutt@drivtrafikk.no>
2017-05-26 11:56:29 +0200

    finished footer

--- core/footer.php
+++ core/footer.php
@@ -6,9 +6,9 @@
 	    		<span>© Westerdals Oslo AC</span>
 	    	</div>
 	    	<div class="footer-inner-right">
-	    		<a href="#"><img class="footer-inner-right-icon" src="img/icons/instagram.png" alt="instagram logo"/></a>
-	    		<a href="#"><img class="footer-inner-right-icon" src="img/icons/twitter.png" alt="twitter logo"/></a>
-	    		<a href="#"><img class="footer-inner-right-icon" src="img/icons/facebook.png" alt="facebook logo"/></a>
+	    		<a href="https://www.instagram.com/attcampus/" target="_blank"><img class="footer-inner-right-icon" src="img/icons/instagram.png" alt="instagram logo"/></a>
+	    		<a href="https://twitter.com/attcampus" target="_blank"><img class="footer-inner-right-icon" src="img/icons/twitter.png" alt="twitter logo"/></a>
+	    		<a href="https://www.facebook.com/AtCampusW/?notif_t=fbpage_fan_invite&notif_id=1495791326915829" target="_blank"><img class="footer-inner-right-icon" src="img/icons/facebook.png" alt="facebook logo"/></a>
 	    	</div>
 	    </div>
 	</div>
