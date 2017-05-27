966710db65ac45e769f25a73821e94d1c053e530
Hakon Schutt <hakon.schutt@drivtrafikk.no>
2017-05-27 16:11:00 +0200

    small changes

--- core/footer.php
+++ core/footer.php
@@ -8,7 +8,7 @@
 	    	<div class="footer-inner-right">
 	    		<a href="https://www.instagram.com/westerdalsact" target="_blank"><img class="footer-inner-right-icon" src="img/icons/instagram.png" alt="instagram logo"/></a>
 	    		<a href="https://twitter.com/westerdalsact" target="_blank"><img class="footer-inner-right-icon" src="img/icons/twitter.png" alt="twitter logo"/></a>
-	    		<a href="https://www.facebook.com/westerdalsosloact" target="_blank"><img class="footer-inner-right-icon" src="img/icons/facebook.png" alt="facebook logo"/></a>
+	    		<a href="https://www.facebook.com/westerdalsosloact " target="_blank"><img class="footer-inner-right-icon" src="img/icons/facebook.png" alt="facebook logo"/></a>
 	    	</div>
 	    </div>
 	</div>
